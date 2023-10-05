1fb67d46854d5144d6519b367a7a0ca29b0fd088
Ali Aldawoodi <124013342+Ali-Aldawoodi@users.noreply.github.com>
2023-10-04 19:15:55 -0600

    Added server.js file and in it the required middleware and libraries

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./constrollers');
+
+const sequelize = require('./configure/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'Very secret',
+    cookie: {
+        maxAge: 500000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(_dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
