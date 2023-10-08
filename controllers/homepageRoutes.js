6c99e0756f1554432448f45b2e4a161acd4ee70f
Ali Aldawoodi <124013342+Ali-Aldawoodi@users.noreply.github.com>
2023-10-08 15:28:00 -0600

    Created controller files. Created index file to call proper middleware as well as homepage routes to get and render the blogposts

---
+++ controllers/homepageRoutes.js
@@ -0,0 +1,77 @@
+const router = require('express').Router();
+const { BlogPost, User } = require('../models')
+const withAuth = require('../utils/withAuth')
+
+router.get('/', async (req, res) => {
+    try {
+        const blogPostData = await BlogPost.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+
+        const blogPosts = blogPostData.map((blogPost) => blogPost.get({ plain: true }));
+
+        res.render('homepage', {
+            blogPosts,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/blogpost/:id', async (req, res) => {
+    try {
+        const blogPostData = await BlogPost.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+        
+        const blogpost = blogPostData.get({ plain: true });
+
+        res.render('blogpost', {
+            ...blogpost,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/blogpost', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password']},
+            include: [{ model: BlogPost }],
+        });
+
+        const user = userData.get({ plain : true });
+
+        res.render('profile', {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}); 
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/profile');
+        return;
+    }
+
+    res.render('login');
+});
+
+module.exports = router;
---
+++ models/BlogPost.js
@@ -0,0 +1,38 @@
+// WHEN I click on an existing blog post
+// THEN I am presented with the :
+// post title, contents, post creator’s username, and date created for that post and have the option to leave a comment
+
+const sequelize = require ('../configure/connection');
+const {Model, DataTypes} = require('sequelize');
+
+class BlogPost extends Model {}
+
+BlogPost.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    content: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    date_created: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+    }
+}); 
+
+module.exports = BlogPost;
+
+
