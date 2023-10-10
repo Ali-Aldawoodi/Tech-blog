aeac71f67fa705b7fa94897d0709bb6e282e8502
Ali Aldawoodi <124013342+Ali-Aldawoodi@users.noreply.github.com>
2023-10-09 20:03:06 -0600

    Added comment model

--- models/index.js
+++ models/index.js
@@ -1,12 +1,22 @@
 const User = require('./User');
 const BlogPost = require('./BlogPost')
+const Comment = require('./Comment')
+
 
 User.hasMany(BlogPost, {
-    foreignKey: 'user_id',
+    foreignKey: 'user_id'
 })
 
 BlogPost.belongsTo(User, {
     foreignKey: 'user_id'
-});
+})
 
-module.exports = {User, BlogPost}
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+})
+
+module.exports = {User, BlogPost, Comment}
